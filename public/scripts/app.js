9ada8cbcae518dc1245be10b66c7bad558d0be3f
Two-Watch-Boy <twowatchboy@gmail.com>
2016-05-24 09:20:30 -0700

    lots of additions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,26 +8,26 @@ angular
 config.$inject = ['$routeProvider'];
 function config(   $routeProvider   ) {
   $routeProvider
-    .when('/home', {
-      templateUrl: '/public/templates/home.html',
-      // controller: 'AboutController',
-      controllerAs: 'homeCtrl'
-    })
-    .when('/about', {
-      templateUrl: '/public/templates/about.html',
-      // controller: 'SpecialController',
-      controllerAs: 'aboutCtrl'
-    })
-    .when('/work', {
-      templateUrl: '/public/templates/work.html',
-      // controller: 'PicturesController',
-      controllerAs: 'workCtrl'
-    })
-    .when('/contact', {
-      templateUrl: '/public/templates/contact.html',
-      // controller: 'ContactController',
-      controllerAs: 'contactCtrl'
-    })
+  .when('/home', {
+    templateUrl: '/public/templates/home.html',
+    // controller: 'AboutController',
+    controllerAs: 'homeCtrl'
+  })
+  .when('/about', {
+    templateUrl: '/public/templates/about.html',
+    // controller: 'SpecialController',
+    controllerAs: 'aboutCtrl'
+  })
+  .when('/work', {
+    templateUrl: '/public/templates/work.html',
+    // controller: 'PicturesController',
+    controllerAs: 'workCtrl'
+  })
+  .when('/contact', {
+    templateUrl: '/public/templates/contact.html',
+    // controller: 'ContactController',
+    controllerAs: 'contactCtrl'
+  })
 
-    .otherwise({redirectTo: '/home'});
-  }
+  .otherwise({redirectTo: '/home'});
+}
